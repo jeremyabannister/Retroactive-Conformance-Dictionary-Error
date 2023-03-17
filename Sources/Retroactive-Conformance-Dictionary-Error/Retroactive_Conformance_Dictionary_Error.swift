c1ea27fbef69026ba5eee673709b6e15679ad412
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-03-17 13:43:53 +0100

    Added retroactive conditional conformance `Dictionary: Error where Value: Error`.

--- Sources/Retroactive-Conformance-Dictionary-Error/Retroactive_Conformance_Dictionary_Error.swift
+++
@@ -1,6 +0,0 @@
-public struct Retroactive_Conformance_Dictionary_Error {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Tests/Retroactive-Conformance-Dictionary-Error-tests/_exported_imports.swift
@@ -0,0 +1,9 @@
+//
+//  _exported_imports.swift
+//  
+//
+//  Created by Jeremy Bannister on 3/17/23.
+//
+
+///
+@_exported import Retroactive_Conformance_Dictionary_Error
